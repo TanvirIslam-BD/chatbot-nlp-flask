317009625fe7c7ed58418e944a557ed5914b6695
tanvirul <tanvirul@bitmascot.com>
2024-09-27 23:21:11 +0600

    chatbot webapp implementation

--- app.py
+++ app.py
@@ -11,10 +11,17 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 conversation_history = []
 
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
 @app.route('/chatbot', methods=['POST'])
 def handle_prompt():
     data = request.get_data(as_text=True)
     data = json.loads(data)
+    print(data)  # DEBUG
     input_text = data['prompt']
 
     # Create conversation history string
@@ -24,7 +31,7 @@ def handle_prompt():
     inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
 
     # Generate the response from the model
-    outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
+    outputs = model.generate(**inputs)
 
     # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
@@ -34,6 +41,7 @@ def handle_prompt():
     conversation_history.append(response)
 
     return response
+
 
 if __name__ == '__main__':
     app.run()
